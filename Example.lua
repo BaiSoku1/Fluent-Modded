cb5e6034c4a40c4fa9ad880d678ac6f440271741
BaiSoku1 <baisoku24@gmail.com>
2026-04-26 23:20:39 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/BaiSoku1/Fluent-Modded/refs/heads/master/Fluent/Main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
